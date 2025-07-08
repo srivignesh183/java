3dbdb7056f3b492b73ff590892baf5fee40a4975
srivignesh183 <srivignesh.s2024cce@sece.ac.in>
2025-07-08 12:04:06 +0530

    Create basic prgm of .java

---
+++ basic prgm of .java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int tomato = 2;
+	    int weight_machine=0;
+	    weight_machine=tomato;
+		System.out.print(weight_machine);
+	
+	}
+}

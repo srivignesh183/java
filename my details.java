07baddfe3c761ad0921bece136c675b4116aa63b
srivignesh183 <srivignesh.s2024cce@sece.ac.in>
2025-07-09 09:29:21 +0530

    Create my details.java

---
+++ my details.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String name="sri vignesh",street="vadasithurroad",city="kinathukadavu",state="tamilnadu",country="india";//string data type declared
+	   
+		System.out.println("name = "+name);
+			System.out.println("street = "+street);
+				System.out.println("city = "+city);
+					System.out.println("state = "+state);
+						System.out.println("country = "+country);
+	}
+}

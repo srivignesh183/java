ce6f9a20ed0227e46bbee7af13c591bbd4d6343d
srivignesh183 <srivignesh.s2024cce@sece.ac.in>
2025-07-09 11:26:40 +0530

    Create boolean.java

---
+++ boolean.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+//java program to print the age
+/*Author is sri vignesh
+*Date=09.07.2025
+*/
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    boolean gate=false; 
+		System.out.println("gate = "+gate);
+	}
+}

03873660911791188c334f3eb70cceddb6587382
srivignesh183 <srivignesh.s2024cce@sece.ac.in>
2025-07-09 10:11:01 +0530

    Create input in .java

---
+++ input in .java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    Scanner stringScanner= new Scanner(System.in); 
+		String name,street,city,country;
+		name="sri vignesh";
+		street="anna salai";
+		city="coimbatore";
+		country=stringScanner.nextLine();
+		System.out.println("Name ="+name+"\n"+"Street ="+street+"\n"+"city ="+city+"\n"+"country ="+country+"\n");
+	}
+}
